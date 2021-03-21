2cfe290c972d38d1ecb822d17e3630272066df15
Hao <haaochi@users.noreply.github.com>
2021-03-21 15:23:29 +0100

    Add clickable operators

--- main.js
+++ main.js
@@ -1,23 +1,44 @@
 const input_display = document.querySelector('.result');
 const button_all = document.querySelectorAll('.numpad > .number');
 const clear = document.querySelector('.ac');
-console.log(clear);
+const button_op = document.querySelectorAll('.operator');
+console.log(button_op);
 
 let first_num = '';
+let second_num = '';
+let operators = {
+    '+': function (a, b, c) { return a + b + c }
+};
+let btn_press = null;
+let saved_op = '';
 
-for (const btn_press of button_all) {
+
+
+for (btn_press of button_all) {
     btn_press.addEventListener('click', (e) => {
-        let btn_target = e.target.value;
-        console.log(e.target)
-        first_num += btn_target;
-        input_display.setAttribute('value', first_num);
+        if (saved_op !== '') {
+            console.log('Now second number')
+        } else {
+            let btn_target = e.target.value;
+            first_num += btn_target;
+            input_display.setAttribute('value', first_num);
+        }
     });
 };
 
+for (const op_press of button_op) {
+    op_press.addEventListener('click', (e) => {
+        const op_target = e.target.value;
+        saved_op = op_target;
+
+    })
+};
+
+
 clear.addEventListener('click', () => {
     first_num = '';
+    saved_op = '';
     input_display.setAttribute('value', first_num);
-    console.log(first_num);
 })
 
 
